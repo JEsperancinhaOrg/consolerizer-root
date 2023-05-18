a66277070da998199b0ac219678e2edd37e249e6
Joao Esperancinha <jofisaes@gmail.com>
2023-05-18 07:08:29 +0200

    removes modularization

--- consolerizer/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module consolerizer {
-    exports org.jesperancinha.console.consolerizer.console;
-    exports org.jesperancinha.console.consolerizer.common;
-    exports org.jesperancinha.console.consolerizer.html;
-    requires org.apache.commons.text;
-    requires com.fasterxml.jackson.databind;
-}
